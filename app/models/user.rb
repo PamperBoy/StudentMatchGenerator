8cccbebe482d92a31d356d3c177b7411f43370b6
pamperboy <marc_smalbrugge@live.nl>
2018-01-03 14:05:03 +0100

    StudentGroups dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :student_groups
+  has_many :student_groups, dependent: :destroy
 end
