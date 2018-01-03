e85458fa85e5d97c65909310e9ddaa17452a3425
pamperboy <marc_smalbrugge@live.nl>
2018-01-03 16:18:23 +0100

    display group members for student

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -2,11 +2,18 @@ class UserController < ApplicationController
 
   def show
     @user = current_user
+    @groups = StudentGroup.all
 
-    if @user.admin
-      @current_user_groups = StudentGroup.all
-    else
-      @current_user_groups = StudentGroup.where(user_id: @user.id).all
+
+    if @user.admin == false
+      @current_user_groups = @groups.where(user_id: @user.id).all
     end
   end
+
+  def get_student_group(id)
+    @groups.where(group_id: id).all
+  end
+
+  helper_method :get_student_group
+
 end
